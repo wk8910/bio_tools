fdb9a1f92df795a716b0f89fbf71032428922e9c
Kun Wang <wk8910@gmail.com>
2017-02-25 09:00:55 +0800

    read fasta

---
+++ 00.scripts/readingFasta_NoBioPerl.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+# This script is used to reading fasta file without the help of BioPerl
+use strict;
+use warnings;
+
+my $file=shift;
+$/=">";
+
+open(I,"< $file");
+while (<I>) {
+    chomp;
+    my @lines=split("\n",$_);
+    next if(@lines==0);
+    my $id=shift @lines;# the name of fasta is $id
+    my $seq=join "",@lines;# the sequence of fasta is $seq
+    print ">$id\n$seq\n";
+}
+close I;
