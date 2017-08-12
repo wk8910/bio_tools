87dbc7f11a835b8cc1de58ea9727ba1377677ff3
Kun Wang <wk8910@gmail.com>
2017-08-12 17:01:02 +0800

    gemma

---
+++ 17.gwas/03.gemma/01.basic.sh
@@ -0,0 +1,3 @@
+~/software/gemma/gemma.linux -b prune -lm 1 -o simple
+~/software/gemma/gemma.linux -b prune -gk 1 -o kingship
+~/software/gemma/gemma.linux -b prune -k output/kingship.cXX.txt -lmm 1 -o lmm
